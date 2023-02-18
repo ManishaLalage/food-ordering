8eadd05608ed41cad40d90d245d9a71c8b3d2a78
ManishaLalage <manishalalage91@gmail.com>
2023-02-18 07:59:41 -0600

    Made Navbar and card

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+// import Tp from './Tp';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    {/* <Tp /> */}
+    <App />
+  </React.StrictMode>
+);
+

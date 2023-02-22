ac48744b33c8b263c9f0a36dc77a39b5e2418c37
ManishaLalage <manishalalage91@gmail.com>
2023-02-21 22:31:42 -0600

    We added search bar

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import "./App.css"
+
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-// import Tp from './Tp';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Body from './components/Body';
+// import Body from './components/RestaurantCard';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    {/* <Tp /> */}
-    <App />
-  </React.StrictMode>
+  <>
+ {/* < h1>hello</h1> */}
+
+      <Header />
+      <Body />
+      <Footer />
+    </>
 );
 

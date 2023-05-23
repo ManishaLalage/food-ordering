c110e87a26657eba4e3bda9bbcb249681abcbda1
ManishaLalage <manishalalage91@gmail.com>
2023-05-22 22:28:49 -0600

    Chapter 7 added

--- src/index.js
+++ src/index.js
@@ -1,19 +1,54 @@
-import React from 'react';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Header from "./components/Header";
+import Body from "./components/Body";
+import Footer from "./components/Footer";
+import About from "./components/About";
+import Error from "./components/Error";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import Contact from "./components/Contact";
+import RestaurantMenu from "./components/RestrauntMenu";
 import "./App.css"
 
-import ReactDOM from 'react-dom/client';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Body from './components/Body';
-// import Body from './components/RestaurantCard';
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <>
- {/* < h1>hello</h1> */}
-
+const AppLayout = () => {
+  return (
+    <>
       <Header />
-      <Body />
+      {/* <Body/> */}
+      <Outlet />
       <Footer />
     </>
-);
+  );
+};
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/",
+        element: <Body />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "/restaurant/:resId",
+        element: <RestaurantMenu />,
+      },
+    ],
+  },
+]);
 
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<RouterProvider router={appRouter} />);
+
+
+// root.render(<AppLayout />);
